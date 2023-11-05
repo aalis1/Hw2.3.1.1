88ef69ec2b324a5ccf90fa660a68836e2cbcf29a
aalis1 <aalisva1@gmail.com>
2023-11-05 15:01:34 +0000

    hw 2.3.1.1 plyers

---
+++ hw 2.3.1.1.js
@@ -0,0 +1,10 @@
+let command = {
+  Ivan: 4500,
+  Petr: 3600,
+  Vadim: 3433,
+  Olga: 2356,
+};
+for (elementKey in command) {
+  console.log(...(elementKey + command[elementKey]));
+  return Math.max({ ...(command[elementKey] + elementKey) });
+}
