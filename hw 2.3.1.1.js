4af8a8ce73bf757ff1c8a0a56ca3af0b8a45ddf0
aalis1 <aalisva1@gmail.com>
2023-11-07 18:31:35 +0000

    create testData

--- hw 2.3.1.1.js
+++ hw 2.3.1.1.js
@@ -1,10 +1,14 @@
-let command = {
-  Ivan: 4500,
-  Petr: 3600,
-  Vadim: 3433,
-  Olga: 2356,
-};
-for (elementKey in command) {
-  console.log(...(elementKey + command[elementKey]));
-  return Math.max({ ...(command[elementKey] + elementKey) });
-}
+const groopPlayers = require("./testData.js");
+
+console.log(groopPlayers);
+
+// let command = {
+//   Ivan: 4500,
+//   Petr: 3600,
+//   Vadim: 3433,
+//   Olga: 2356,
+// };
+// for (elementKey in command) {
+//   console.log(...(elementKey + command[elementKey]));
+//   return Math.max({ ...(command[elementKey] + elementKey) });
+// }
